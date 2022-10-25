e5dead442220f665216320da3bc27b1c1e687d05
Hercules de Sousa Silva <hercules.sousa@academico.ifpb.edu.br>
2022-10-25 16:08:56 -0300

    Criando classes iniciais

---
+++ src/Raizes.java
@@ -0,0 +1,2 @@
+public class Raizes {
+}
---
+++ src/Sistema.java
@@ -0,0 +1,56 @@
+public class Sistema implements FuncionalidadesIF {
+    @Override
+    public Usuario cadastraUsuario(String nome, String email, String senha) {
+        return null;
+    }
+
+    @Override
+    public Raizes calculaFuncaoSegundoGrau(double a, double b, double c) {
+        return null;
+    }
+
+    @Override
+    public boolean ehTriangulo(double angulo1, double angulo2, double angulo3) {
+        return false;
+    }
+
+    @Override
+    public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3) {
+        return false;
+    }
+
+    @Override
+    public boolean ehRetanuglo(double lado1, double lado2, double lado3, double lado4) {
+        return false;
+    }
+
+    @Override
+    public double perimetroRetangulo(double lado1, double lado2, double lado3, double lado4) {
+        return 0;
+    }
+
+    @Override
+    public double areaRetangulo(double lado1, double lado2, double lado3, double lado4) {
+        return 0;
+    }
+
+    @Override
+    public double calculadora(String expressao) {
+        return 0;
+    }
+
+    @Override
+    public double areaCirculo(double raio) {
+        return 0;
+    }
+
+    @Override
+    public double areaPerimetro(double raio) {
+        return 0;
+    }
+
+    @Override
+    public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2) {
+        return 0;
+    }
+}
