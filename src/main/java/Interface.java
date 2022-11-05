d39f5621154c213fc6163777be7fe1e01987cef9
Hercules de Sousa Silva <hercules.sousa@academico.ifpb.edu.br>
2022-11-05 16:03:18 -0300

    Refatorando menu

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,19 +1,68 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Interface {
-    public static void showMenu() {
+    public static String mostrarMenu(ArrayList<String> opcoes) {
         System.out.println("Olá, pessoal. Qual das nossas funcionalidades você gostaria de utilizar?");
-        String menu = "   \n" +
-                "   1. Cadastrar usuário\n" +
-                "   2. Calcular equação de segundo grau\n" +
-                "   3. Verificar lados de um triângulo\n" +
-                "   4. Classificar triângulo\n" +
-                "   5. Verificar lados de um triângulo retângulo\n" +
-                "   6. Validar retângulo\n" +
-                "   7. Calcular área e perímetro de um retângulo\n" +
-                "   8. Calculadora\n" +
-                "   9. Calcular área e perímetro de um círculo\n" +
-                "   10. Calcular distância entre dois pontos\n" +
-                "   11. Sair\n";
+
+        StringBuilder menu = new StringBuilder("   \n");
+        for(int i = 1; i <= opcoes.size(); i++) {
+            menu.append(String.format("   %d. %s\n", i, opcoes.get(i - 1)));
+        }
+
         System.out.println(menu);
         System.out.println("Escolha alguma das opções acima:");
+
+        Scanner scanner = new Scanner(System.in);
+        String opcao = scanner.nextLine();
+        while (true) {
+            try {
+                int intOpcao = Integer.parseInt(opcao);
+                if (intOpcao > 0 && intOpcao <= opcoes.size()) {
+                    break;
+                }
+                else {
+                    System.out.println();
+                    System.out.println("Opção inválida");
+                    System.out.println(menu);
+                    System.out.println("Escolha alguma das opções acima:");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println();
+                System.out.println("Opção inválida");
+                System.out.println(menu);
+                System.out.println("Escolha alguma das opções acima:");
+            }
+
+            scanner = new Scanner(System.in);
+            opcao = scanner.nextLine();
+        }
+
+        return opcao;
+    }
+
+    public static String mostrarMenuSimOuNao() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Você deseja tentar novamente o cadastro?");
+        System.out.println("""
+                                  \s
+                                   1. Sim
+                                   2. Não
+                                """);
+        String resposta = scanner.nextLine();
+        while (!resposta.equals("1") && !resposta.equals("2")) {
+            System.out.println();
+            System.out.println("Por favor, digite uma entrada válida");
+            System.out.println();
+            System.out.println("Você deseja tentar novamente o cadastro?");
+            System.out.println("""
+                                  \s
+                                   1. Sim
+                                   2. Não
+                                """);
+            resposta = scanner.nextLine();
+        }
+        return resposta;
     }
 }
--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,20 +9,25 @@ public class MainApplication {
 
         sistema.setup();
 
-        Interface.showMenu();
-        String entrada = scanner.nextLine();
+        ArrayList<String> opcoes = new ArrayList<>();
+        opcoes.add("Cadastrar clientes");
+        opcoes.add("Calcular equação de segundo grau");
+        opcoes.add("Verificar lados de um triângulo");
+        opcoes.add("Classificar triângulo");
+        opcoes.add("Verificar lados de um triângulo retângulo");
+        opcoes.add("Validar retângulo");
+        opcoes.add("Calcular área e perímetro de um retângulo");
+        opcoes.add("Calculadora");
+        opcoes.add("Calcular área e perímetro de um círculo");
+        opcoes.add("Calcular distância entre dois pontos");
+        opcoes.add("Sair");
 
+        boolean isMostarMenu = true;
+        while (isMostarMenu) {
+            String opcaoEscolhida = Interface.mostrarMenu(opcoes);
 
-        while (!entrada.equals("11")) {
-            // Código para limpar o console
-            // Não funciona em uma IDE
-            // System.out.print("\033[H\033[2J");
-            // System.out.flush();
-
-            System.out.println();
-
-            switch (entrada) {
-                case "1":
+            switch (opcaoEscolhida) {
+                case "1" -> {
                     System.out.println("Vamos cadastrar um novo cliente.");
                     System.out.println("Para realizar essa operação vamos precisar de algumas informações.");
                     System.out.println("Digite o nome do usuário:");
@@ -30,7 +36,6 @@ public class MainApplication {
                     String email = scanner.nextLine();
                     System.out.println("Digite a senha do usuário:");
                     String senha = scanner.nextLine();
-
                     Usuario usuario;
                     try {
                         usuario = sistema.cadastraUsuario(nome, email, senha);
@@ -38,18 +43,25 @@ public class MainApplication {
                         System.out.println("O usuário foi criado com sucesso");
                         System.out.println(usuario);
                         System.out.println();
-                    }
-                    catch (SenhaInvalidaException e) {
-                        System.out.println();
-                        System.out.println("A senha digitada é inválida");
-                        System.out.println();
                     } catch (EmailInvalidoException e) {
                         System.out.println();
                         System.out.println("O email digitado é inválido");
                         System.out.println();
+                        String resposta = Interface.mostrarMenuSimOuNao();
+                        if (resposta.equals("1")) {
+                            continue;
+                        }
+                    } catch (SenhaInvalidaException e) {
+                        System.out.println();
+                        System.out.println("A senha digitada é inválida");
+                        System.out.println();
+                        String resposta = Interface.mostrarMenuSimOuNao();
+                        if (resposta.equals("1")) {
+                            continue;
+                        }
                     }
-                    break;
-                case "7":
+                }
+                case "7" -> {
                     Scanner sc = new Scanner(System.in);
                     System.out.print("Digite os valores dos lados do retângulo: \n");
                     try{
@@ -68,8 +80,8 @@ public class MainApplication {
                     catch (Exception e){
                         System.out.println("Não foi possível calcular o perímetro. Verifique os valores e tente novamente.");
                     }
-                    break;
-                case "8":
+                }
+                case "8" -> {
                     System.out.print("Digite a expressão desejada (Ex.: 6 * 2): ");
                     String operacao = scanner.nextLine();
                     try{
@@ -84,7 +96,8 @@ public class MainApplication {
                         System.out.println("A operação é inválida. Tente realizar a operação com outros números.");
                     }
                     break;
-                case "9":
+                }
+                case "9" -> {
                     Scanner sc1 = new Scanner(System.in);
                     System.out.print("Digite o valor do raio do círculo: ");
                     try{
@@ -98,7 +111,8 @@ public class MainApplication {
                         System.out.println("Apenas valores maiores do que zero são permitidos ;)");
                     }
                     break;
-                case "10":
+                }
+                case "10" -> {
                     try {
                         Scanner sc2 = new Scanner(System.in);
                         System.out.print("Digite as coordenadas do ponto 1 (x1 y1) separados por espaço: (Ex.: 0 3) ");
@@ -116,11 +130,9 @@ public class MainApplication {
                     catch(ArrayIndexOutOfBoundsException | NumberFormatException e){
                         System.out.println("Coordenadas inválidas. Digite as coordenadas de cada ponto separado por espaço (Ex.: 0 3)");
                     }
-
-
+                }
+                case "11" -> isMostarMenu = false;
             }
-            Interface.showMenu();
-            entrada = scanner.nextLine();
         }
     }
 }
