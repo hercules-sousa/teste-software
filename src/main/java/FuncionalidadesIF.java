ccbca94e46b10cd62aafdfebb78b875908d93185
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2022-11-06 17:55:41 -0300

    Implementando mais opções do menu.

--- src/main/java/FuncionalidadesIF.java
+++ src/main/java/FuncionalidadesIF.java
@@ -2,9 +2,9 @@ public interface FuncionalidadesIF {
     public Usuario cadastraUsuario(String nome, String email, String senha) throws EmailInvalidoException, SenhaInvalidaException;
     public Raizes calculaFuncaoSegundoGrau(double a, double b, double c);
     public boolean ehTriangulo(double angulo1, double angulo2, double angulo3);
-    public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3);
-    public String classificaTriangulo(double angulo1, double angulo2, double angulo3);
-    public boolean ehRetangulo(double lado1, double lado2, double lado3, double lado4);
+    public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3) throws Exception;
+    public String classificaTriangulo(double angulo1, double angulo2, double angulo3) throws Exception;
+    public boolean ehRetangulo(double lado1, double lado2, double lado3, double lado4) throws Exception;
     public double perimetroRetangulo(double lado1, double lado2, double lado3, double lado4) throws Exception;
     public double areaRetangulo(double lado1, double lado2, double lado3, double lado4) throws Exception;
     public double calculadora(String expressao);
